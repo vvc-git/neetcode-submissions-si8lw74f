23be8db0f5839920a7c7f48e370fde54ce4252ec
Victor do Valle <78426009+vvc-git@users.noreply.github.com>
2026-04-17 16:09:10 -0300

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!set.contains(i)) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
