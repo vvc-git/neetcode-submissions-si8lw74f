a59bc04b2c062376f8363ca9bf34d321f93304a4
Victor do Valle <78426009+vvc-git@users.noreply.github.com>
2026-04-22 08:08:57 -0300

    Add: jump-game - submission-13

---
+++ Data Structures & Algorithms/jump-game/submission-13.java
@@ -0,0 +1,24 @@
+class Solution {
+    int[] nums;
+    int n;
+
+
+    public boolean dfs(int idx) {
+        if (idx == n - 1) return true;
+        if ((idx >= n) || (idx < n && nums[idx] == 0)) return false;
+
+        for (int i = nums[idx]; i > 0; i--) {
+            if (i >= n)
+                continue;
+            if (dfs( i + idx)) return true;
+        }
+        return false;
+    }
+
+
+    public boolean canJump(int[] nums) {
+        this.nums = nums;
+        this.n = nums.length;
+        return dfs(0);
+    }
+}
